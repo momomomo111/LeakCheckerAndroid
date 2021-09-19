8c7df6e324e906e80254c6d625bffc80ee71cfe0
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2021-09-19 19:29:32 +0900

    [add] added HomeScreen
    ホームスクリーンを追加

--- app/src/main/java/com/moasanuma/leakchecker/MainActivity.kt
+++ app/src/main/java/com/moasanuma/leakchecker/MainActivity.kt
@@ -3,11 +3,7 @@ package com.moasanuma.leakchecker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import com.moasanuma.leakchecker.ui.HomeScreen
 import com.moasanuma.leakchecker.ui.theme.LeakCheckerTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LeakCheckerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LeakCheckerTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/moasanuma/leakchecker/ui/HomeScreen.kt
@@ -0,0 +1,68 @@
+package com.moasanuma.leakchecker.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen() {
+    var pass by remember { mutableStateOf("") }
+    Surface {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "調べたいパスワードを\n入力してください",
+                style = typography.h5
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            TextField(
+                value = pass,
+                onValueChange = { pass = it },
+                singleLine = true,
+                label = { Text("パスワード") }
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(onClick = { /*TODO*/ }) {
+                Text(
+                    text = "調べる",
+                    style = typography.h5
+                )
+            }
+            Spacer(modifier = Modifier.height(64.dp))
+            Text(
+                text = "※パスワードは暗号化されて\nから送信されます",
+                style = typography.h6,
+                color = Color.Gray
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewHomeScreen() {
+    HomeScreen()
+}
