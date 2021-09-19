eef5a9444c86312f662bb084b37a20709ae3ee85
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2021-09-19 22:31:57 +0900

    [add] create ApiModule
    ApiModuleを作成

---
+++ app/src/main/java/com/moasanuma/leakchecker/network/ApiModule.kt
@@ -0,0 +1,55 @@
+package com.moasanuma.leakchecker.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+    @Provides
+    fun provideHeaderInterceptor(): Interceptor {
+        return Interceptor {
+            it.request().newBuilder()
+                .build()
+                .let(it::proceed)
+        }
+    }
+
+    @Provides
+    fun provideOkHttpClient(interceptor: Interceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .addInterceptor(
+                HttpLoggingInterceptor()
+                    .apply { HttpLoggingInterceptor.Level.BODY }
+            )
+            .build()
+    }
+
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient, moshi: Moshi): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.pwnedpasswords.com/")
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+}
