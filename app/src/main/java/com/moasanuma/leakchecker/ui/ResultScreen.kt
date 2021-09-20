e2b8d4cc0ac2568a2d4890bd1dea208f6fd83a37
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2021-09-20 15:08:21 +0900

    [clean] Migrate string to stringResource
    文字列をstringResourceに移行

--- app/src/main/java/com/moasanuma/leakchecker/ui/ResultScreen.kt
+++ app/src/main/java/com/moasanuma/leakchecker/ui/ResultScreen.kt
@@ -2,6 +2,7 @@ package com.moasanuma.leakchecker.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme.typography
@@ -12,11 +13,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.moasanuma.leakchecker.R
 import com.moasanuma.leakchecker.viewmodel.PassViewModel
 
 @Composable
@@ -37,12 +40,19 @@ fun ResultScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
+            Row(verticalAlignment = Alignment.Bottom) {
+                Text(
+                    text = result,
+                    style = typography.h2
+                )
+                Text(
+                    text = stringResource(R.string.leak_num),
+                    style = typography.h4
+                )
+            }
+
             Text(
-                text = result + "件",
-                style = typography.h2
-            )
-            Text(
-                text = "の脅威が発見されました",
+                text = stringResource(R.string.detecting_threats),
                 style = typography.h5
             )
         }
