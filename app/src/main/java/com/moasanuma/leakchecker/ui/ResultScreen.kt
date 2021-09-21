bb1fdc6ff94dcf29cba21acef77d8a2d15d992ce
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2021-09-21 11:56:41 +0900

    [add] Add waveAnimation for the result screen.
    結果画面のwaveAnimationを追加

--- app/src/main/java/com/moasanuma/leakchecker/ui/ResultScreen.kt
+++ app/src/main/java/com/moasanuma/leakchecker/ui/ResultScreen.kt
@@ -5,14 +5,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,6 +25,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.moasanuma.leakchecker.R
+import com.moasanuma.leakchecker.ui.components.WaveAnimation
 import com.moasanuma.leakchecker.viewmodel.PassViewModel
 
 @Composable
@@ -32,7 +38,9 @@ fun ResultScreen(
         passViewModel.getLeakPassList(pass)
     }
     val result: Int by passViewModel.leakNum.observeAsState(-2)
+    var waveColor: Color by remember { mutableStateOf(Color.Gray) }
     Surface {
+        WaveAnimation(Modifier, waveColor, 0.4F)
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -49,10 +57,13 @@ fun ResultScreen(
                     text = stringResource(R.string.communication_error_messages),
                     style = typography.h5
                 )
-                0 -> Text(
-                    text = stringResource(R.string.leak_not_found),
-                    style = typography.h5
-                )
+                0 -> {
+                    Text(
+                        text = stringResource(R.string.leak_not_found),
+                        style = typography.h5
+                    )
+                    waveColor = colors.secondary
+                }
                 else -> {
                     Row(verticalAlignment = Alignment.Bottom) {
                         Text(
@@ -68,6 +79,7 @@ fun ResultScreen(
                         text = stringResource(R.string.detecting_threats),
                         style = typography.h5
                     )
+                    waveColor = colors.error
                 }
             }
         }
